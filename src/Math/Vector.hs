404c5b0fa536b64f798ee4d869976a60068c2e37
Christoffer Lind Andersen <57995582+Chri692u@users.noreply.github.com>
2023-07-01 18:21:14 +0200

    Show implementation

--- src/Math/Vector.hs
+++ src/Math/Vector.hs
@@ -5,6 +5,8 @@ import Data.Array
 import Math.Matrix
 
 type Vector a = Array Int a
+showV :: Show a => Vector a -> String
+showV = unlines . map show . elems
 
 -- Convert a vector to a matrix
 toMatrix :: Vector a -> Matrix a
@@ -19,6 +21,7 @@ fromMatrix matrix
     | rows matrix == 1 = array (1, cols matrix) $ zip [1..] (map (\i -> elements matrix ! (1, i)) [1..cols matrix])
     | otherwise = error "VECTOR LENGTH ERROR"
 
+-- todo: from list
 
 -- Function for binary operators on vectors
 binop :: (a -> a -> a) -> Vector a -> Vector a -> Vector a
@@ -43,6 +46,10 @@ dot :: Num a => Array Int a -> Array Int a -> a
 dot v1 v2
   | bounds v1 /= bounds v2 = error "VECTOR LENGTH ERROR"
   | otherwise = sum [v1 ! i * v2 ! i | i <- indices v1]
+
+-- reverse a vector
+hat :: Vector a -> Vector a
+hat vec = listArray (bounds vec) (reverse (elems vec))
 
 -- Projection with normalization
 projection :: (Eq a, Fractional a) => Array Int a -> Array Int a -> Array Int a
